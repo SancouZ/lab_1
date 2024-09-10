bb8a7adf7066e88b275c2e25fc035b971f3c8171
Ms black <li304ko3@gmail.com>
2024-09-10 20:19:33 +0300

    feat: added formula for A, 1 exercise

--- lab_1/1.cpp
+++ lab_1/1.cpp
@@ -3,6 +3,12 @@
 
 int main()
 {
-	std::cout << " ";
+	int k, m;
+	double r, d;
+	double a;
+	std::cout << "input k,m,r,d: ";
+	std::cin >> k >> m >> r >> d;
+	a = (tan(k / (2 + r))) + (sqrt((exp(r + 1)) / (fabs(pow(d, 5) + 1))));
+	std::cout << a;
 	return 0;
 }
