01ec53e5c519760f004a2288b8080a3a80a5ef63
Ms black <161579194+Ms1black@users.noreply.github.com>
2024-09-11 10:03:29 +0300

    feat: add <iomanip> with function

--- lab_1/1.cpp
+++ lab_1/1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 int main()
 {
@@ -11,8 +12,8 @@ int main()
 	a = (tan(k / (2 + r))) + (sqrt((exp(r + 1)) / (fabs(pow(d, 5) + 1))));
 	b = sin(pow(k, 2) + 3) + 2 * (pow(cos(k * m), 3));
 	c = ((pow(log(2 + pow(k, 2)), 3) + pow(log(2 + pow(k, 3)), 2)) / 3) + pow((tan(k/r)/ (2 + 7.5 * sin(d))), 2);
-	std::cout << "A = " << a << std::endl;
-	std::cout << "B = " << b << std::endl;
-	std::cout << "C = " << c << std::endl;
+	std::cout << "A = " << std::setw(8) << std::setprecision(3) << a << std::endl;
+	std::cout << "B = " << std::setw(8) << std::setprecision(3) << b << std::endl;
+	std::cout << "C = " << std::setw(8) << std::setprecision(3) << c << std::endl;
 	return 0;
 }
